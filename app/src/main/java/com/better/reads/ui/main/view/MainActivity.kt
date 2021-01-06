e73a01c05bfd243d41d921dc39c09b9b1867fc90
Richard Raguine <richard.raguine@ffuf.de>
2021-01-06 18:11:48 +0800

    fix: layout tag with databinding enabled

--- app/src/main/java/com/better/reads/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/better/reads/ui/main/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.better.reads.ui.main.view
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -25,11 +26,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_main, null, false)
-        // binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
         //setContentView(R.layout.activity_main)
+        // val decorView: View = window.decorView
+        // val contentView = decorView.findViewById<View>(android.R.id.content) as ViewGroup
+        //binding = DataBindingUtil.inflate(layoutInflater, R.layout.activity_main, contentView, false)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        ///setContentView(binding.root)
+        // binding = ActivityMainBinding.inflate(layoutInflater)
+        //
         setupUI()
         setupViewModel()
         setupObserver()
